38ee355eddbd8997123a9243e321f3578b4f8378
Gonzalo Candia <gonzaloismael.cg@gmail.com>
2023-07-07 22:46:25 -0300

    Lista de categorías

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 function Nav() {
-  return (
-    <>
-      <p>🥑</p>
-      <ul>
-        <li>
+ 
+return (
+  <nav className={styles.navbar}>
+    <Link to="/">
+      <div className={styles.logo}>
+        <img src="/logo.png" alt="Logo" className={styles.logoImg} />
+      </div>
+    </Link>
+    <ul>
+      <li>
           <Link to="/">Home</Link>
-        </li>
-      </ul>
-    </>
-  );
-}
+      </li>
+      <li>
+        <Link to="/personajes">Personajes</Link>
+      </li>
+      <li>
+        <Link to="/ubicaciones">Ubicaciones</Link>
+      </li>
+      <li>
+        <Link to="/episodios">Episodios</Link>
+      </li>
+    </ul>
+  </nav>
+);
+};
+
 
 export default Nav;
--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -1,47 +1,48 @@
-import Artist from "../../components/Artist";
-import Search from "../../components/Search";
-import styles from "./styles.module.css";
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
 
-const QUERY_KEY_CHARACTERS = "characters";
-
-const fetchCharacters = async () => {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
-  const json = await res.json();
-
-  if (json.error) {
-    throw new Error(json.error);
-  }
-
-  return json.results;
-};
-
-function Home() {
-  const { data, status, error } = useQuery(
-    QUERY_KEY_CHARACTERS,
-    fetchCharacters
-  );
-
-  return (
-    <main>
-      <h1 className={styles.container}>Music Home</h1>
-      <Search />
-      {status === "loading" && <h1>Cargando...</h1>}
-      {status === "error" && <h1>Error: {error.message}</h1>}
-      {status === "success" &&
-        data.map((character) => {
-          return (
-            <Artist
-              key={character.id}
-              id={character.id}
-              name={character.name}
-              song={character.status}
-              views={character.episode.length}
-            />
-          );
-        })}
-    </main>
-  );
+interface Category {
+  id: number;
+  name: string;
+  image: string;
+  creationAt: string;
+  updatedAt: string;
 }
 
-export default Home;
+const App = () => {
+  const { data, isLoading, error } = useQuery<Category[]>('categories', async () => {
+    const response = await fetch('https://api.escuelajs.co/api/v1/categories');
+    if (!response.ok) {
+      throw new Error('Failed to fetch categories');
+    }
+    const data = await response.json();
+    return data;
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className={styles.categoryList}>
+      <h2>Categories</h2>
+      <div className={styles.categoryCards}>
+        {data.map((category) => (
+          <div key={category.id} className={styles.categoryCard}>
+            <Link to={`/category/${category.id}/products`}>
+              <img src={category.image} alt={category.name} />
+              <h3>{category.name}</h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
