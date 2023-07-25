ae1dfb146adf96c965c4b5203b19d9bedd5d21da
Gonzalo Candia <gonzaloismael.cg@gmail.com>
2023-07-24 21:38:35 -0300

    Un poco de limpieza

--- src/app/components/Nav/index.tsx
+++ src/app/components/Nav/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import LogoutModal from "../LogoutModal";
 import Cart from "../Cart";
